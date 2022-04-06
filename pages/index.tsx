d7d13054f59ba01a874dbf03d23021bc93ca5241
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-04-06 11:06:40 +0430

    feat(pages/home): Learn Box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import classnames from 'classnames';
 
 import { BsSearch } from "react-icons/bs";
@@ -56,9 +57,45 @@ const Home: NextPage = () =>
                     <Image src="/static/book.gif" alt="IntelligentQuantum German Word" width={350} height={350} />
                 </span>
                 <section className={styles.sectionHomeLevel}>
-                    <div className={styles.sectionHomeLevelBoxA}>A</div>
-                    <div className={styles.sectionHomeLevelBoxC}>C</div>
-                    <div className={styles.sectionHomeLevelBoxB}>B</div>
+                    <div className={styles.sectionHomeLevelBoxA}>
+                        <span>
+                            <Image src="/static/a.png" alt="IntelligentQuantum German Word" width={130} height={130} />
+                        </span>
+
+                        <p>
+                            You can find commonly used sentences and phrases related to everyday situations (eg personal and family information, shopping, local geography, environment). It can be used in normal tasks that require simple exchange and information in the familiar and ordinary. You can explain your history, environment and issues of urgent need.
+                        </p>
+
+                        <Link href="/">
+                            <a>I want to learn</a>
+                        </Link>
+                    </div>
+                    <div className={styles.sectionHomeLevelBoxB}>
+                        <span>
+                            <Image src="/static/b.png" alt="IntelligentQuantum German Word" width={130} height={130} />
+                        </span>
+
+                        <p>
+                            You can find commonly used sentences and phrases related to everyday situations (eg personal and family information, shopping, local geography, environment). It can be used in normal tasks that require simple exchange and information in the familiar and ordinary. You can explain your history, environment and issues of urgent need.
+                        </p>
+
+                        <Link href="/">
+                            <a>I want to learn</a>
+                        </Link>
+                    </div>
+                    <div className={styles.sectionHomeLevelBoxC}>
+                        <span>
+                            <Image src="/static/c.png" alt="IntelligentQuantum German Word" width={130} height={130} />
+                        </span>
+
+                        <p>
+                            You can find commonly used sentences and phrases related to everyday situations (eg personal and family information, shopping, local geography, environment). It can be used in normal tasks that require simple exchange and information in the familiar and ordinary. You can explain your history, environment and issues of urgent need.
+                        </p>
+
+                        <Link href="/">
+                            <a>I want to learn</a>
+                        </Link>
+                    </div>
                 </section>
             </Main>
         </>
